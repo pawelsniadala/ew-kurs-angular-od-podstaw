b49178cd33c1c44d8e393ebda7a2bfc1a61cfe55
Paweł Śniadała <biner.home@gmail.com>
2023-04-14 03:11:52 +0200

    Interaktywny formularz

--- portfolio/src/app/components/welcome/welcome.component.ts
+++ portfolio/src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,37 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//     selector: 'app-welcome',
+//     templateUrl: './welcome.component.html',
+//     styleUrls: ['./welcome.component.css']
+// })
+
+// export class WelcomeComponent {
+//     user = {
+//         firstName: 'Handsome Jack',
+//         isSubscrined: true,
+//         birthday: new Date(1970,1,1)
+//     }
+
+//     askAboutName = true
+
+//     dateFormat = 'mediumDate'
+
+//     askIfSubscribed() {
+//         return this.user.isSubscrined
+//             ? 'You are subscribed'
+//             : 'Want to get updates ' + this.user.firstName + '?';
+//     }
+
+//     changeFirstName(firstNameRef: HTMLInputElement) {
+//         this.user.firstName = firstNameRef.value;
+//     }
+// }
+
+import {
+    Component,
+    OnInit
+} from '@angular/core';
 
 @Component({
     selector: 'app-welcome',
@@ -6,24 +39,50 @@ import { Component } from '@angular/core';
     styleUrls: ['./welcome.component.css']
 })
 
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
     user = {
-        firstName: 'Handsome Jack',
-        isSubscrined: true,
-        birthday: new Date(1970,1,1)
+        firstName: 'Handsome',
+        isSubscribed: false,
+        email: ''
     }
 
-    askAboutName = true
-
-    dateFormat = 'mediumDate'
-
-    askIfSubscribed() {
-        return this.user.isSubscrined
-            ? 'You are subscribed'
-            : 'Want to get updates ' + this.user.firstName + '?';
+    steps = {
+        showWelcome: false,
+        askForFirstName: true,
+        askForEmail: false,
+        askToSubscribe: false,
+        subscribedSuccess: false
     }
 
-    changeFirstName(firstNameRef: HTMLInputElement) {
-        this.user.firstName = firstNameRef.value;
+    constructor() {}
+
+    updateName(firstName: string) {
+        this.user.firstName = firstName;
+
+        if('Handsome'.startsWith(firstName) === false) {
+            this.steps.showWelcome = true;
+        }
+
+        if(this.steps.showWelcome && firstName.length >= 3) {
+            setTimeout(() => {
+                this.steps.askToSubscribe = true;
+            }, 1500);
+        }
     }
+
+    agreedToSubscribe() {
+        this.steps.askForEmail = true;
+        this.steps.askToSubscribe = false;
+    }
+
+    subscribeUser(email: string) {
+        this.user.email = email;
+        this.user.isSubscribed = true;
+        this.steps.askForEmail = false;
+        this.steps.subscribedSuccess = true;
+    }
+
+    skipSubscription() {}
+
+    ngOnInit(): void {}
 }
