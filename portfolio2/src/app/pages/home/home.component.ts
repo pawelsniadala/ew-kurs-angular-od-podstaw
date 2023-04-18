94e009926adc7c608a9c6c4402ff94b0b4ccca95
Paweł Śniadała <biner.home@gmail.com>
2023-04-18 12:04:28 +0200

    Wyswietlanie dynamicznych kolekcji

--- portfolio2/src/app/pages/home/home.component.ts
+++ portfolio2/src/app/pages/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Project {
+  id: number,
+  name: string,
+  image: string
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,4 +15,21 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   readmore: boolean = false;
   currentTab: string = '';
+  recentProject: Project[] = [
+    {
+      id: 1,
+      name: 'Project One',
+      image: 'https://picsum.photos/seed/256/400/400'
+    },
+    {
+      id: 2,
+      name: 'Project Two',
+      image: 'https://picsum.photos/seed/953/400/400'
+    },
+    {
+      id: 2,
+      name: 'Project Three',
+      image: 'https://picsum.photos/seed/971/400/400'
+    }
+  ];
 }
